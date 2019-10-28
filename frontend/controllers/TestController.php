116fdf3797e6546ad9e63de64206cbc251d7504e
Felix1987 <shilenkomp@mail.ru>
2019-10-28 17:53:24 +0300

    not need ch

--- frontend/controllers/TestController.php
+++ frontend/controllers/TestController.php
@@ -2,36 +2,59 @@
 namespace frontend\controllers;
 use Yii;
 use yii\web\Controller;
-use frontend\models\Test;
+use frontend\models\News;
+use Faker\Factory;
+
 class TestController extends Controller
-{
-    public function actionIndex()
+{	
+ public function actionGenerate()
     {
+        ini_set('max_execution_time', 10000);
         
-        $max = Yii::$app->params['maxNewsInList'];
+        /* @var $faker Faker\Generator */
+        $faker = Factory::create();
         
-        $list = Test::getNewsList($max);
+        for ($j = 0; $j < 1000; $j++) {
+            
+            $news = [];
+            for ($i = 0; $i < 100; $i++) {
+                $news[] = [$faker->text(35), $faker->text(rand(1000, 2000)), rand(0, 1)];
+            }
+            Yii::$app->db->createCommand()->batchInsert('news', ['title', 'content', 'status'], $news)->execute();
+            unset($news);
+            
+        }
+
+        return $this->render('generate');
+    }
+	
+    //~ public function actionIndex()
+    //~ {
+        
+        //~ $max = Yii::$app->params['maxNewsInList'];
+        
+        //~ $list = Test::getNewsList($max);
                 
-        return $this->render('index', [
-            'list' => $list,
-        ]);
-    }
+        //~ return $this->render('index', [
+            //~ 'list' => $list,
+        //~ ]);
+    //~ }
     
-    public function actionView($id)
-    {
-        $item = Test::getItem($id);
-        return $this->render('view', [
-            'item' => $item 
-        ]);
-    }    
-    public function actionMail()
-    {
-        $result = Yii::$app->mailer->compose()
-                ->setFrom('shilenkomihail1987@gmail.com')
-                ->setTo('shilenkomihail1987@gmail.com')
-                ->setSubject('Тема сообщения')
-                ->setTextBody('Текст сообщения')
-                ->setHtmlBody('<b>текст сообщения в формате HTML</b>')
-                ->send();
-    }
+    //~ public function actionView($id)
+    //~ {
+        //~ $item = Test::getItem($id);
+        //~ return $this->render('view', [
+            //~ 'item' => $item 
+        //~ ]);
+    //~ }    
+    //~ public function actionMail()
+    //~ {
+        //~ $result = Yii::$app->mailer->compose()
+                //~ ->setFrom('shilenkomihail1987@gmail.com')
+                //~ ->setTo('shilenkomihail1987@gmail.com')
+                //~ ->setSubject('Тема сообщения')
+                //~ ->setTextBody('Текст сообщения')
+                //~ ->setHtmlBody('<b>текст сообщения в формате HTML</b>')
+                //~ ->send();
+    //~ }
 }
---
+++ frontend/models/News.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "news".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $content
+ * @property int $status
+ */
+class News extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'news';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content', 'status'], 'required'],
+            [['content'], 'string'],
+            [['status'], 'integer'],
+            [['title'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'status' => 'Status',
+        ];
+    }
+}
